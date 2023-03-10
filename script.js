e6c1da2044409a845cfaf1e0422eefbe8c016124
sestradarya <nguendarina1@gmail.com>
2023-03-10 23:59:27 +0300

    we

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ document.addEventListener('click', (event) => {
         if(event.target.innerHTML === 'AC'){
             clearInputScreen()
         }
+        else if(event.target.innerHTML === 'CE'){
+
+        }
+        else if(event.target.innerHTML === '='){
+            getResult()
+        }
         else{
             inputScreen.textContent += event.target.innerHTML
         }
@@ -15,3 +21,12 @@ document.addEventListener('click', (event) => {
 function clearInputScreen(){
     inputScreen.innerHTML = ' '
 }  
+
+function deleteLast
+
+function getResult(){
+    const textExpr = inputScreen.innerHTML;
+    const expr = eval(textExpr);
+    historyScreen.innerHTML = textExpr;
+    inputScreen.innerHTML = expr;
+}
