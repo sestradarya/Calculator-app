1e8f8dad5142e0d5e21bcefc49aaeeaba81cf865
sestradarya <nguendarina1@gmail.com>
2023-03-11 02:08:09 +0300

    details

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-const historyScreen = document.getElementById('history')
-const inputScreen = document.getElementById('input')
+const historyScreen = document.getElementById("history");
+const inputScreen = document.getElementById("input");
 
-document.addEventListener('click', (event) => {
-    if(event.target.localName === 'button'){
-        if(event.target.innerHTML === 'AC'){
-            clearInputScreen()
-        }
-        else if(event.target.innerHTML === 'CE'){
-
-        }
-        else if(event.target.innerHTML === '='){
-            getResult()
-        }
-        else{
-            inputScreen.textContent += event.target.innerHTML
-        }
+document.addEventListener("click", (event) => {
+  if (event.target.localName === "button") {
+    if (event.target.innerHTML === "AC") {
+      clearInputScreen();
+    } else if (event.target.innerHTML === "CE") {
+      deleteLastCharacter();
+    } else if (event.target.innerHTML === "%") {
+      inputScreen.innerHTML += event.target.innerHTML;
+      calculatePercent();
+    } else if (event.target.innerHTML === "=") {
+      getResult();
+    } else {
+      inputScreen.innerHTML += event.target.innerHTML;
     }
-})
+  }
+});
 
-function clearInputScreen(){
-    inputScreen.innerHTML = ' '
-}  
+function clearInputScreen() {
+  inputScreen.innerHTML = " ";
+  historyScreen.innerHTML = " ";
+}
 
-function deleteLast
+function deleteLastCharacter() {
+  const expr = inputScreen.innerHTML;
+  inputScreen.innerHTML = expr.slice(0, -1);
+}
 
-function getResult(){
+function calculatePercent() {
+  const expr = inputScreen.innerHTML;
+  const partialValue = expr.matchAll(/\d+(?=%)/);
+  console.log(partialValue);
+}
+
+function getResult() {
+  try {
     const textExpr = inputScreen.innerHTML;
     const expr = eval(textExpr);
     historyScreen.innerHTML = textExpr;
     inputScreen.innerHTML = expr;
+  } catch (error) {
+    
+  }
 }
